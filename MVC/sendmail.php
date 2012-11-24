8cb8c9c075d09e99db463737a770cc306c02d593
Yoshiyuki Sugimoto <s.yosiyuki@gmail.com>
2012-11-25 00:36:17 +0700

    remove comments and require functions.php

--- MVC/sendmail.php
+++ MVC/sendmail.php
@@ -1,18 +1,9 @@
 <?php
-//echo "AAAAAAAAAAAAAAAA";
 require_once(dirname(__FILE__) . '/config.php');
 require_once(dirname(__FILE__) . "/Model.php");
 require_once(dirname(__FILE__) . "/libs/TnkMail.php");
-require_once(dirname(__FILE__) . '/libs/aaa.php');
+require_once(dirname(__FILE__) . '/libs/functions.php');
 
-//echo "BBBBBBBBBBBBBBBBB";
-/*if(isset($_POST['reply'])){
-    foreach ($_POST['reply'] as $key => $val) {
-        $mailId =  $key;
-    }*/
-
-
-//echo "BBCCCCCCCCCCCBBBB";
 if (! isset($_GET['id'])) {
 //    echo "no mail with id";
     exit();
@@ -21,22 +12,10 @@ if (! isset($_GET['id'])) {
 $dbconnect = new Model(DB_HOST, DB_NAME, DB_USER, DB_PASS);
 $mailArr = $dbconnect->getMailById($_GET['id']);
 
-//var_dump($mailArr);
 if (! $mailArr) {
     echo "no mail with id";
     exit();
 }
-
-//echo "DDDDDDDDDDDBBBBBB";
-//var_dump($mailArr);
-
-
-
-//echo "tesdt";
-
-
-
-//var_dump($_POST);
 
 if(isset($_POST['submit'])){
     $mail = $dbconnect->getMailById($_GET['id']);
